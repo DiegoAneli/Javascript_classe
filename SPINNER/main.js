0627304094b523ba820c89f780fc216efbe20247
DiegoAneli <diegoaneli@gmail.com>
2026-05-27 16:02:04 +0200

    pedro done

--- SPINNER/main.js
+++ SPINNER/main.js
@@ -9,6 +9,9 @@ const spinner = document.getElementById('spinner');
 //anzichè fare una funzione classica
 //posso associare un addeventlistner all id passando un click
 
+stopBtn.disabled = true;
+
+
 
 //pulsante start
 startBtn.addEventListener("click", () => {
@@ -16,7 +19,11 @@ startBtn.addEventListener("click", () => {
     //al click, rimuovo la classe d-none allo spinner preso da id
     spinner.classList.remove("d-none");
 
+    //disabilito il play
     startBtn.disabled = true;
+
+    //attivo il pulsante di Stop
+    stopBtn.disabled = false;
 
 
     //voglio un icona spinner per il caricamento al click
@@ -24,6 +31,16 @@ startBtn.addEventListener("click", () => {
 
     setTimeout( () => {
 
+        //rimuovere lo spinner
+        spinner.classList.add('d-none');
+
+
+        //ruoto l immagine
+        immagine.classList.add("rotate");
+
+
+        //inizia la musica
+        musica.play();
 
 
 
@@ -41,6 +58,19 @@ stopBtn.addEventListener("click", () => {
     //al click, aggiungo la classe d-none allo spinner preso da id
     spinner.classList.add("d-none");
 
+    //arresto musica
+    musica.pause();
+    musica.currentTime = 0; //riporto la canzone all inizio
+
+    //abilito il bottone di play
+    startBtn.disabled = false;
+
+    //disabilitae se stesso quindi di stop
+    stopBtn.disabled = true;
+
+
+    //fermo  immagine
+    immagine.classList.remove("rotate");
 
 });
 
