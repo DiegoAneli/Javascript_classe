6e4931f40afee4c0f465eaf4827c8b45a2394b9e
DiegoAneli <diegoaneli@gmail.com>
2026-06-03 15:30:07 +0200

    api v.3

---
+++ API/poke.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Esercizio poke api
+ * 
+ * è richiesto di andare a costruire una lista di li che verranno appesi ad una lista
+ * 
+ * renderizzare il name 
+ * 
+ * "species": {
+    "name": "ditto",
+    "url": "https://pokeapi.co/api/v2/pokemon-species/132/"
+  },
+
+
+  ciclare su held_item , verificando che se contiene almeno un oggetto
+  iteriamo con foreach su ciascun oggetto e estraiamo 
+  
+  il nome dell oggetto
+  e per ogni evrsione mostriamo "rarity"
+
+ * 
+ */
